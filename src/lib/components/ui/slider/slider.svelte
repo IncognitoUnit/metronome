3bc6e44d6bbfa47f38accb0eb976e4c5f6d3a2ee
IncognitoUnit <81387110+IncognitoUnit@users.noreply.github.com>
2025-03-20 13:17:11 +0400

    feat: refactor Slider component to support multiple thumbs and ticks rendering

--- src/lib/components/ui/slider/slider.svelte
+++ src/lib/components/ui/slider/slider.svelte
@@ -12,12 +12,18 @@
 	class={cn('relative flex w-full touch-none items-center select-none', className)}
 	{...restProps}
 >
-	<span class="bg-secondary relative h-2 w-full grow overflow-hidden rounded-full">
-		<SliderPrimitive.Range class="bg-primary absolute h-full" />
-	</span>
-	<SliderPrimitive.Thumb
-		index={0}
-		class="border-primary bg-background ring-offset-background focus-visible:ring-ring block h-5 w-5 rounded-full border-2 transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50"
-	/>
-	<SliderPrimitive.Tick index={0} />
+	{#snippet children({ ticks, thumbs })}
+		<span class="bg-secondary relative h-2 w-full grow overflow-hidden rounded-full">
+			<SliderPrimitive.Range class="bg-primary absolute h-full" />
+		</span>
+		{#each thumbs as index (index)}
+			<SliderPrimitive.Thumb
+				{index}
+				class="border-primary bg-background ring-offset-background focus-visible:ring-ring block h-5 w-5 rounded-full border-2 transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50"
+			/>
+		{/each}
+		{#each ticks as index (index)}
+			<SliderPrimitive.Tick {index} />
+		{/each}
+	{/snippet}
 </SliderPrimitive.Root>
