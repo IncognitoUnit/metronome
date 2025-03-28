47a1c44e1cc2bcc15b37cfbf4c9aea306032bccb
IncognitoUnit <81387110+IncognitoUnit@users.noreply.github.com>
2025-03-28 16:50:47 +0400

    feat: add separator component

---
+++ src/lib/components/ui/separator/separator.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { Separator as SeparatorPrimitive, type SeparatorRootProps } from 'bits-ui';
+
+	import { cn } from '$lib/utils.js';
+
+	let {
+		orientation = 'horizontal',
+		decorative = false,
+		class: className,
+		...restProps
+	}: SeparatorRootProps = $props();
+</script>
+
+<SeparatorPrimitive.Root
+	class={cn(
+		'bg-border shrink-0',
+		orientation === 'horizontal' ? 'h-[1px] w-full' : 'min-h-full w-[1px]',
+		className,
+	)}
+	{orientation}
+	{decorative}
+	{...restProps}
+/>
